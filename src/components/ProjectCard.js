1ae87f12e55107ee1c658dc5402d245777e45bad
Chubop <samuelokirby@gmail.com>
2023-01-23 14:40:22 -0500

    responsive project card title

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -11,7 +11,7 @@ export default function ProjectCard(props){
         <>
             <Box sx={{width: width < 1300 ? '80vw' : '50vw', paddingBottom: 1, marginBottom: 8, backgroundImage: "linear-gradient(to bottom, #1e1e1e, #171717)", borderRadius: '10px'}}>
                 <ProjectImage src={props.src}/>
-                <Typography variant="h4" sx={{padding: 4, paddingBottom: 0, paddingTop: 3}}>
+                <Typography variant={width < 1300 ? 'h5' : 'h4'} sx={{padding: 4, paddingBottom: 0, paddingTop: 3}}>
                     {props.title}
                 </Typography>
                 <Typography variant="h6" sx={{padding: 4, color: 'lightgray', paddingTop: 2}}>
