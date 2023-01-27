0a4a0d2b6110f498c86e351585b7ec87ed2a21cf
Chubop <samuelokirby@gmail.com>
2023-01-27 12:50:09 -0500

    increased thickness of menu button font

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -38,7 +38,7 @@ export default function ImageCard(props){
                     }}>
                     <Typography variant={width >= 900 ? 'h4' : 'h5'} 
                     sx={{width: '100%', 
-                    fontWeight: 200,
+                    fontWeight: 400,
                     transform: mouseEntered && 'scale(1.10)',
                     transition: mouseEntered && 'all 0.2s ease-in-out',
                     textShadow: '-1px -1px 3 gray'}}>
